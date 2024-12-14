62697cdf841ede562ac3260bd779e1f8a2af3b92
Daniel Catalan <dancatalan415@gmail.com>
2024-12-13 22:55:29 -0800

    working solution for part 2

--- day7/src/operator.rs
+++ day7/src/operator.rs
@@ -35,16 +35,62 @@ impl Iterator for OpSetIter {
     }
 }
 
+pub struct OpSetIter2 {
+    length: usize,
+    state: usize,
+}
+
+impl OpSetIter2 {
+    pub fn new(length: usize) -> Self {
+        let length = length - 1;
+        OpSetIter2 { length, state: 0 }
+    }
+}
+
+impl Iterator for OpSetIter2 {
+    type Item = Vec<Ops>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let limit = 3_usize.pow(self.length as u32);
+        let mut state = self.state;
+        if state >= limit {
+            return None;
+        }
+
+        let mut ops = vec![Ops::Add];
+
+        for _ in 0..self.length {
+            let x = state % 3;
+            let op = match x {
+                0 => Ops::Add,
+                1 => Ops::Mult,
+                2 => Ops::ConCat,
+                _ => panic!("unexpect value for state % 3"),
+            };
+            state = state / 3;
+            ops.push(op);
+        }
+
+        self.state += 1;
+        Some(ops)
+    }
+}
+
 #[derive(Debug)]
 pub enum Ops {
     Add,
     Mult,
+    ConCat,
 }
 impl Ops {
     pub fn calc(&self, a: &usize, b: &usize) -> usize {
         match self {
             Ops::Add => a + b,
             Ops::Mult => a * b,
+            Ops::ConCat => {
+                let s = format!("{a}{b}");
+                s.parse().unwrap()
+            }
         }
     }
 }
--- day7/src/part2.rs
+++ day7/src/part2.rs
@@ -4,6 +4,12 @@ use once_cell::sync::Lazy;
 use regex::Regex;
 use std::io::BufRead;
 
+use crate::{
+    equation::Equation,
+    operator::{OpSetIter, OpSetIter2},
+    parse::parse_to_equations,
+};
+
 /* Notes
  *
  * for regex use Lazy struct.
@@ -12,26 +18,55 @@ use std::io::BufRead;
  *
  */
 
-pub fn solve_solution<R: BufRead>(_reader: R) -> u32 {
-    1
+pub fn solve_solution<R: BufRead>(reader: R) -> usize {
+    let x: usize = reader
+        .lines()
+        .map(|line| parse_to_equations(&line.unwrap()))
+        .filter(|e| possibly_true(&e))
+        .map(|e| e.expected)
+        .sum();
+
+    x
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use std::{io::BufReader, str::FromStr};
+fn possibly_true(eq: &Equation) -> bool {
+    let len = eq.num_len();
 
-//     #[test]
-//     fn test_solve() {
-//         let input = String::from_str(
-//             "some
-// lines
-// of
-// text",
-//         )
-//         .unwrap();
-//         let reader = BufReader::new(input.as_bytes());
-//         let _solution = solve_solution(reader);
-//         todo!("write an assertion")
-//     }
-// }
+    let opset_iter = OpSetIter2::new(len);
+    let mut valid_counter = 0;
+    for ops in opset_iter {
+        if eq.valid_ops(&ops) {
+            valid_counter += 1;
+        }
+    }
+
+    match valid_counter {
+        0 => false,
+        i32::MIN..=-1_i32 | 1_i32..=i32::MAX => true,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{io::BufReader, str::FromStr};
+
+    #[test]
+    fn test_solve() {
+        let input = String::from_str(
+            "190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20",
+        )
+        .unwrap();
+        let reader = BufReader::new(input.as_bytes());
+        let solution = solve_solution(reader);
+        assert_eq!(11387, solution)
+    }
+}
